891d5cedc4c78ce9b94b9e5b23e0834f8b14e182
KalvinVilla <pro.kavika@pm.me>
2025-10-21 13:09:33 +0200

    chore: remove unused code

--- src/types/main.ts
+++ src/types/main.ts
@@ -1,11 +1,4 @@
-import type {
-  PostgresDialect,
-  SqliteDialect,
-  MysqlDialect,
-  LogEvent,
-  // ControlledTransaction,
-} from 'kysely'
-// import type { DB } from 'adonis-kysely/types/db'
+import type { PostgresDialect, SqliteDialect, MysqlDialect, LogEvent } from 'kysely'
 
 export type LogLevel = 'query' | 'error'
 
@@ -14,14 +7,4 @@ export type LogConfig = LogLevel[] | ((event: LogEvent) => void)
 export type AdonisKyselyConfig = {
   dialect: PostgresDialect | SqliteDialect | MysqlDialect
   log?: LogConfig
-  advanced: {
-    bypass_test_security: boolean
-  }
 }
-
-// export interface TransactionData {
-//   connection: ControlledTransaction<DB>
-//   savepoint?: ControlledTransaction<DB>
-//   parentId?: string
-//   depth: number
-// }
