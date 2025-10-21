bad78f450517cc50f3d69dfe432a9bf15e5cbf67
Kavika <69009197+KalvinVilla@users.noreply.github.com>
2025-10-21 13:39:26 +0200

    feat: add basic command (#13)
    This commit contains basic commands : 
    
    - seed
    - migration
    - db type

--- commands/make_seeder.ts
+++ commands/make_seeder.ts
@@ -3,23 +3,24 @@ import type { CommandOptions } from '@adonisjs/core/types/ace'
 
 import { stubsRoot } from '../stubs/main.js'
 
-export default class MakeMigration extends BaseCommand {
-  static commandName = 'make:migration'
-  static description = 'Create a new migration file (kysely)'
+export default class MakeSeeder extends BaseCommand {
+  static commandName = 'make:seeder'
+  static description = 'Make a new seeder file (kysely)'
   static options: CommandOptions = {}
 
-  @args.string({ description: 'Name of the migration file' })
+  @args.string({ description: 'Name of the seed file' })
   declare name: string
 
   async run() {
     const entity = this.app.generators.createEntity(this.name)
     const tableName = this.app.generators.tableName(entity.name)
-    const fileName = `${new Date().getTime()}_create_${tableName}_table.ts`
+
+    const fileName = `${tableName}_seeder.ts`
 
     const codemods = await this.createCodemods()
-    await codemods.makeUsingStub(stubsRoot, 'commands/make/migration.stub', {
+    await codemods.makeUsingStub(stubsRoot, 'commands/make/seeder.stub', {
       entity,
-      migration: {
+      seeder: {
         tableName,
         fileName,
       },
---
+++ commands/migration_rollback.ts
@@ -0,0 +1,87 @@
+import type { CommandOptions } from '@adonisjs/core/types/ace'
+import type { AdonisKyselyDB } from '#src/kysely_db'
+
+import * as fs from 'node:fs/promises'
+import * as path from 'node:path'
+
+import { BaseCommand, flags } from '@adonisjs/core/ace'
+import { FileMigrationProvider, Migrator } from 'kysely'
+
+export default class MigrateRollback extends BaseCommand {
+  static commandName = 'migration:rollback'
+  static description =
+    'Rollback the database by running down method on the migration files (kysely)'
+  static options: CommandOptions = {
+    startApp: true,
+  }
+
+  @flags.number({ description: 'Define number of migrations to rollback', default: 1, alias: 's' })
+  declare step: number
+
+  declare migrator: Migrator
+
+  declare database: AdonisKyselyDB
+
+  /**
+   * Prepare lifecycle hook runs before the "run" method
+   * and hence, we use it to prepare the migrator
+   * instance
+   */
+  async prepare() {
+    this.database = (await this.app.container.make('adonis-kysely')) as AdonisKyselyDB
+    this.migrator = new Migrator({
+      db: this.database.getConnexion(),
+      provider: new FileMigrationProvider({
+        fs,
+        path,
+        migrationFolder: this.app.migrationsPath(),
+      }),
+    })
+  }
+
+  /**
+   * The complete lifecycle hook runs after the "run" method
+   * and hence, we use it to close the data connection.
+   */
+  async completed() {
+    await this.database.destroy()
+  }
+
+  /**
+   * Runs migrations down method
+   */
+  async run() {
+    for (let i = 0; i < this.step; i++) {
+      await this.#migrateDown()
+    }
+  }
+
+  async #migrateDown() {
+    const { error, results } = await this.migrator.migrateDown()
+
+    /**
+     * Print results
+     */
+    results?.forEach((it) => {
+      switch (it.status) {
+        case 'Success':
+          this.logger.success(`migration "${it.migrationName}" rolled back successfully`)
+          break
+        case 'Error':
+          this.logger.error(`failed to rollback migration "${it.migrationName}"`)
+          break
+        case 'NotExecuted':
+          this.logger.info(`rollback pending "${it.migrationName}"`)
+      }
+    })
+
+    /**
+     * Display error
+     */
+    if (error) {
+      this.logger.error('Failed to rollback')
+      this.error = error
+      this.exitCode = 1
+    }
+  }
+}
